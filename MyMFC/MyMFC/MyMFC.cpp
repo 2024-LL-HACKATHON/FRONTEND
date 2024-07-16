34e80351190044d15601e492cec2a8bc068207a8
kyungbinYoo <107679773+kyungbinYoo@users.noreply.github.com>
2024-07-17 08:48:36 +0900

    Add files via upload

---
+++ MyMFC/MyMFC/MyMFC.cpp
@@ -0,0 +1,72 @@
+﻿#include "pch.h"
+#include "MyMFC.h"
+#include "afxdialogex.h"
+#include <iostream>
+#include "resource.h"
+
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+#include "MyMFCDlg.h"
+
+CMyMFCDlg::CMyMFCDlg(CWnd* pParent /*=nullptr*/)
+    : CDialogEx(IDD_MYMFC_DIALOG, pParent)
+{
+    m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMyMFCDlg::DoDataExchange(CDataExchange* pDX)
+{
+    CDialogEx::DoDataExchange(pDX);
+    DDX_Control(pDX, IDC_LIST1, m_listBox);
+}
+
+BEGIN_MESSAGE_MAP(CMyMFCDlg, CDialogEx)
+    ON_BN_CLICKED(IDC_LOAD_BUTTON, &CMyMFCDlg::OnBnClickedLoadButton)
+END_MESSAGE_MAP()
+
+BOOL CMyMFCDlg::OnInitDialog()
+{
+    CDialogEx::OnInitDialog();
+
+    // 여기에 초기화 코드 추가.
+
+    return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CMyMFCDlg::ReadLASFile(const std::string& filename)
+{
+    std::ifstream file(filename, std::ios::binary);
+    if (!file)
+    {
+        AfxMessageBox(_T("Failed to open LAS file."));
+        return;
+    }
+
+    LASPoint point;
+    while (file.read(reinterpret_cast<char*>(&point), sizeof(LASPoint)))
+    {
+        m_points.push_back(point);
+    }
+
+    file.close();
+}
+
+void CMyMFCDlg::OnBnClickedLoadButton()
+{
+    std::string filename = "your_las_file.las";  // LAS 파일 경로 설정
+    ReadLASFile(filename);
+
+    // LAS 포인트 데이터를 텍스트로 출력
+    CString text;
+    for (const auto& point : m_points)
+    {
+        CString temp;
+        temp.Format(_T("x: %.2f, y: %.2f, intensity: %u\n"), point.x, point.y, point.intensity);
+        text += temp;
+    }
+
+    // 리스트 박스에 텍스트 출력
+    m_listBox.AddString(text);
+}
---
+++ MyMFC/MyMFC/MyMFC.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "afxwin.h" // afxwin.h 포함
+
+#include <vector>
+#include <fstream>
+
+// LAS 포인트 구조체 정의
+struct LASPoint
+{
+    double x;
+    double y;
+    unsigned short intensity;
+};
+
+class CMyMFCDlg : public CDialogEx
+{
+public:
+    CMyMFCDlg(CWnd* pParent = nullptr); // 생성자 선언
+    virtual ~CMyMFCDlg(); // 소멸자 선언
+
+#ifdef AFX_DESIGN_TIME
+    enum { IDD = IDD_MYMFC_DIALOG };
+#endif
+
+protected:
+    virtual void DoDataExchange(CDataExchange* pDX); // 데이터 교환 함수 선언
+    virtual BOOL OnInitDialog(); // 초기화 함수 선언
+    DECLARE_MESSAGE_MAP() // 메시지 맵 선언
+
+private:
+    std::vector<LASPoint> m_points; // LAS 포인트를 저장할 벡터
+
+private:
+    void ReadLASFile(const std::string& filename); // LAS 파일을 읽는 함수 선언
+
+public:
+    afx_msg void OnBnClickedLoadButton(); // "Load" 버튼 클릭 이벤트 핸들러 선언
+};
