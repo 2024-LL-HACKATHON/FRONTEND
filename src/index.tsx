34936b2be128558e18df2df3b4470ae9b9cd4b7b
paice <qocodms20010302@gmail.com>
2024-07-23 00:24:33 +0900

    라우트 지정 및 기타 사항 수정

--- src/index.tsx
+++ src/index.tsx
@@ -11,10 +11,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <>
     <GlobalStyle />
     <RouterProvider router={router} />
-    </>
   </React.StrictMode>
 );
 
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,27 +1,27 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Landing from '../pages/Landing/Landing';
-import Signup from '../pages/Singup';
-import Login from '../pages/Login';
-import Guide from '../pages/Guide/Guide';
+import { createBrowserRouter } from "react-router-dom";
+import Landing from "../pages/Landing/Landing";
+import Signup from "../pages/SignUp/SignUp";
+import Login from "../pages/Login/Login";
+import Guide from "../pages/Guide/Guide";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Landing />,
     children: [
-      {
-        path: "login",
-        element: <Login />,
-      },
-      {
-        path: "signup",
-        element: <Signup />,
-      },
     ],
   },
   {
-    path: "guide",
-    element: <Guide />
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/guide",
+    element: <Guide />,
   },
 ]);
 
